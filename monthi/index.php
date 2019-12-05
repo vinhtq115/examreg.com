461d1702d9da8d834c862b2d8245f13f5507e005
vinhtq115 <starcraft6723@hotmail.com>
2019-12-05 13:37:33 +0700

    Fix monthi.

--- monthi/index.php
+++ monthi/index.php
@@ -1,6 +1,6 @@
 <?php
     require_once dirname(__FILE__)."/../account/controller/LogoutController.php";
-    //session_start();
+    session_start();
     if($_SESSION["isAdmin"] != 1){
         header("Location:http://examreg.com/account/view/LogoutView.php");
     }
