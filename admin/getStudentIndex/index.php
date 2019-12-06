7f236431f618c3a8912aec60753dc9605dcda081
nhat <caominhnhat99@gmail.com>
2019-12-06 05:42:23 -0800

    A responsive commit

--- admin/getStudentIndex/index.php
+++ admin/getStudentIndex/index.php
@@ -24,38 +24,75 @@ if($_SESSION["isAdmin"] != 1){
     <link href="/../../bootstrap/bootstrap-3.3.7-dist/css/bootstrap.css" rel="stylesheet"/>
     <link rel="stylesheet" type="text/css" href="/../../css/getStudent.css">
     <link rel="stylesheet" type="text/css" href="/../../css/getStudentTable.css">
+    <link rel="stylesheet" type="text/css" href="/../../css/responsive.css">
 </head>
 <body>
-<div class = "container box">
-<table class="content-table">
-    <thead>
-    <tr>
-        <td>Student ID</td>
-        <td>Family Middle Name</td>
-        <td>Name</td>
-        <td>Date Taking Exam</td>
-        <td>Qualified To Take Exam</td>
-    </tr>
-    </thead>
-    <tbody>
-    <?php
-    $control = new getStudentController();
-    $control->getStudentData();
-    ?>
-    </tbody>
-</table>
-</div>
-<div class="container box">
-<h3 align="center">IMPORT STUDENT FILES</h3></h3><br />
-<form method="POST" enctype="multipart/form-data">
-    <label>Select Excel File</label>
-    <input type="file" name="excel"/>
-    <br />
-    <input type="submit" name="ImportStudent" class="btn btn-info" value="Import Student"/>
-</form>
-<br />
-<br />
+<nav class="navbar">
+    <span class="open-slide">
+      <a href="#" onclick="openSlideMenu()">
+        <svg width="30" height="30">
+            <path d="M0,5 30,5" stroke="#fff" stroke-width="5"/>
+            <path d="M0,14 30,14" stroke="#fff" stroke-width="5"/>
+            <path d="M0,23 30,23" stroke="#fff" stroke-width="5"/>
+        </svg>
+      </a>
+    </span>
+
+    <ul class="navbar-nav">
+        <li><a>Student Management</li>
+    </ul>
+</nav>
+
+<div id="side-menu" class="side-nav">
+    <a href="#" class="btn-close" onclick="closeSlideMenu()">&times;</a>
+    <a href="http://examreg.com/admin/view/AdminView.php" >Admin HomePage</a>
+    <a href="http://examreg.com/account/view/LogoutView.php">Log out</a>
 </div>
 
+<div id="main">
+    <div class = "container box">
+        <table class="content-table">
+            <thead>
+            <tr>
+                <td>Student ID</td>
+                <td>Family Middle Name</td>
+                <td>Name</td>
+                <td>Date Taking Exam</td>
+                <td>Qualified To Take Exam</td>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+//            $control = new getStudentController();
+//            $control->getStudentData();
+            ?>
+            </tbody>
+        </table>
+    </div>
+    <div class="container box">
+        <h3 align="center">IMPORT STUDENT FILES</h3></h3><br />
+        <form method="POST" enctype="multipart/form-data">
+            <label>Select Excel File</label>
+            <input type="file" name="excel"/>
+            <br />
+            <input type="submit" name="ImportStudent" class="btn btn-info" value="Import Student"/>
+        </form>
+        <br />
+        <br />
+    </div>
+</div>
+
+<script>
+    function openSlideMenu(){
+        document.getElementById('side-menu').style.width = '250px';
+        document.getElementById('main').style.marginLeft = '250px';
+    }
+
+    function closeSlideMenu(){
+        document.getElementById('side-menu').style.width = '0';
+        document.getElementById('main').style.marginLeft = '0';
+    }
+</script>
 </body>
+
 </html>
