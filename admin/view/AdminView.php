46436012bad1e4f797cae0f5e8ccae9883c8e344
Vinh Quang Tran <starcraft6723@hotmail.com>
2019-12-09 20:54:23 +0700

    Việt hóa homepage của admin.

--- admin/view/AdminView.php
+++ admin/view/AdminView.php
@@ -8,75 +8,74 @@
     }
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width">
-    <title>Administration's Homepage</title>
+    <title>Trang chủ quản trị viên</title>
     <link rel="stylesheet" type="text/css" href="../../css/adminView.css">
     <script src="../../jquery/jquery-3.4.1.js"></script>
-
 </head>
 <body>
-<header>
-    <div class="container">
-        <div id="branding">
-            <h1><span class="highlight">Exam</span>Reg</h1>
+    <header>
+        <div class="container">
+            <div id="branding">
+                <h1><span class="highlight">Exam</span>Reg</h1>
+            </div>
+            <nav>
+                <ul>
+                    <li class="current">Xin chào <?php echo $_SESSION["id"]?></li>
+                </ul>
+            </nav>
         </div>
-        <nav>
-            <ul>
-                <li class="current"><a id = "logout-btn" href="http://examreg.com/account/view/LogoutView.php">Log out</a></li>
-            </ul>
-        </nav>
-    </div>
-</header>
+    </header>
 
-<section id="showcase">
-    <div class="container">
-        <h1>Administration Of Exam Registration System</h1>
-    </div>
-</section>
+    <section id="showcase">
+        <div class="container">
+            <h1>TRANG QUẢN TRỊ HỆ THỐNG KỲ THI</h1>
+        </div>
+    </section>
 
-<section id="boxes">
-    <div class="container">
-        <div class="box">
-            <a href="http://examreg.com/admin/getStudentIndex/"><img src="../../css/img/graduate-student.png"></a> <!--This will redirect to getting student excel-->
-            <h3>Students Roaster</h3>
-            <p>Administration keeping track and managing student profile which taking part in the examination</p>
+    <section id="boxes">
+        <div class="container">
+            <div class="box">
+                <a href="http://examreg.com/admin/getStudentIndex/"><img src="../../css/img/graduate-student.png"></a> <!--This will redirect to getting student excel-->
+                <h3>Quản lý sinh viên</h3>
+                <p>Trang quản lý danh sách sinh viên, cấp tài khoản, cập nhật trạng thái đủ điều kiện dự thi</p>
+            </div>
+            <div class="box">
+                <a href="http://examreg.com/monthi/"><img src="../../css/img/microscope.png"></a>
+                <h3>Quản lý môn thi</h3>
+                <p>Trang quản lý danh sách môn thi của trường</p>
+            </div>
+            <div class="box">
+                <a href="http://examreg.com/hocphan/"><img src="../../css/img/book.png"></a>
+                <h3>Quản lý học phần</h3>
+                <p>Trang quản lý danh sách học phần của trường</p>
+            </div>
         </div>
-        <div class="box">
-            <a href="http://examreg.com/monthi/"><img src="../../css/img/microscope.png"></a>
-            <h3>Subjects List</h3>
-            <p>Administration keeping track of subjects of the examination</p>
+        <div class="container">
+            <div class="box">
+                <a href="http://examreg.com/kythi"><img src="../../css/img/logo_brush.png"></a>
+                <h3>Quản lý kỳ thi</h3>
+                <p>Trang quản lý danh sách kỳ thi</p>
+            </div>
+            <div class="box">
+                <a href="http://examreg.com/quanlyphongthi"><img src="../../css/img/door.png"></a>
+                <h3>Quản lý phòng thi</h3>
+                <p>Trang quản lý danh sách phòng thi và thông tin từng phòng</p>
+            </div>
+            <div class="box">
+                <a href="http://examreg.com/account/view/LogoutView.php"><img src="../../css/img/clock.png"></a>
+                <h3>Đăng xuất</h3>
+                <p>Thoát khỏi hệ thống ExamReg</p>
+            </div>
         </div>
-        <div class="box">
-            <a href="http://examreg.com/hocphan/"><img src="../../css/img/book.png"></a>
-            <h3>Courses</h3>
-            <p>Administration keeping track of courses of the examination</p>
-        </div>
-    </div>
-    <div class="container">
-        <div class="box">
-            <a href=""><img src="../../css/img/logo_brush.png"></a>
-            <h3>The Exams</h3>
-            <p>Administration creating and managing the upcoming examination </p>
-        </div>
-        <div class="box">
-            <a href=""><img src="../../css/img/door.png"></a>
-            <h3>Exam Room</h3>
-            <p>Administration keep track Exam Rooms and Students in Exam Room</p>
-        </div>
-        <div class="box">
-            <a href="http://examreg.com/account/view/LogoutView.php"><img src="../../css/img/clock.png"></a>
-            <h3>Log Out</h3>
-            <p>Good bye Admin. Hope you have an effective working day</p>
-        </div>
-    </div>
-</section>
+    </section>
 
-<footer>
-    <p>Admin home page</p>
-</footer>
+    <footer>
+        <p>&copy; 2019</p>
+    </footer>
 
 </body>
 </html>
